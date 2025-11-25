1674c9b8271663af33d65c482f19cb9db6218d57
Setthasat Sirikomonsing <setthasat@Setthasats-MacBook-Air.local>
2025-11-25 18:22:54 +0700

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,75 @@
-import Image from "next/image";
+"use client";
+import { useState, useEffect, useRef } from "react";
 
-export default function Home() {
+function Home() {
+  const [count, setCount] = useState(0);
+  const [isPoping, setIsPoping] = useState(false);
+  const [combo, setCombo] = useState(0);
+  const [showCombo, setShowCombo] = useState(false);
+  const [comboAnimation, setComboAnimation] = useState(false);
+
+  const comboTimeout = useRef<NodeJS.Timeout | null>(null);
+
+  const pop = () => {
+    setCount((c) => c + 1);
+    setCombo((c) => c + 1);
+
+    setShowCombo(true);
+    setComboAnimation(true);
+
+    setTimeout(() => {
+      setComboAnimation(false);
+    }, 300);
+
+    setIsPoping(true);
+    setTimeout(() => {
+      setIsPoping(false);
+    }, 100);
+
+    if (comboTimeout.current) clearTimeout(comboTimeout.current);
+
+    comboTimeout.current = setTimeout(() => {
+      setCombo(0);
+      setShowCombo(false);
+    }, 1000);
+  };
+
+  useEffect(() => {
+    const handlekey = (e: KeyboardEvent) => {
+      e.preventDefault();
+      pop();
+    };
+
+    window.addEventListener("keydown", handlekey);
+    return () => window.removeEventListener("keydown", handlekey);
+  }, []);
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div className="w-screen h-screen flex flex-col justify-center items-center bg-pink-300">
+      <img
+        src="cat.png"
+        onClick={pop}
+        className={`w-100 h-100 cursor-pointer transition-transform duration-100 ${
+          isPoping ? "scale-90" : "scale-100"
+        }`}
+        alt="cat"
+      />
+      <p className="mt-34 font-bold text-4xl text-white">{count}</p>
+
+      {showCombo && (
+        <div
+          className={
+            `absolute top-20 text-5xl font-extrabold text-white pointer-events-none transition-all duration-300` +
+            (comboAnimation
+              ? "opacity-100 scale-125 rotate-6"
+              : "opacity-0 scale-75 -rotate-6")
+          }
+        >
+          {combo} combo!!
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+      )}
     </div>
   );
 }
+
+export default Home;
